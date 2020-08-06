64048af606befc085ac23c24825becb3f2afa94d
neko3cs <minaton.0722.net@gmail.com>
2020-08-07 03:55:13 +0900

    ログイン画面デザイン作成

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { GraphComponent } from './graph/graph.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     FooterComponent,
     GraphComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
