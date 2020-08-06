a651868ea11500c3c82aa393e5e3ca173e0cc298
neko3cs <minaton.0722.net@gmail.com>
2020-08-07 02:58:46 +0900

    フッターを最下部に固定

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { GraphComponent } from './graph/graph.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    GraphComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
