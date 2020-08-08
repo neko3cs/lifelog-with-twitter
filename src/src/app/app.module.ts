cff1022450eddc6f19c5f147a89b5a9d077cfc8d
neko3cs <minaton.0722.net@gmail.com>
2020-08-09 01:33:57 +0900

    app.component のフォルダリング

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component/app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { GraphComponent } from './graph/graph.component';
