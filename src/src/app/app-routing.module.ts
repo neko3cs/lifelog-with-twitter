830033295a797004d00825d3a8c864f45cd6659f
neko3cs <minaton.0722.net@gmail.com>
2020-08-09 03:56:39 +0900

    ルーティングの実装

--- src/src/app/app-routing.module.ts
+++ src/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
